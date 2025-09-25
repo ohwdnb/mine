8258d611b18eebf933eeecdb173f4861fdae755d
ohwdnb <2607337376@qq.com>
2025-09-25 17:06:08 +0800

    提交代码

---
+++ t4/t4.c
@@ -0,0 +1,98 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+////	/*int arr[10] = { 0 };
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(arr[0]));
+//	printf("%d\n", sizeof(arr) / sizeof(arr[0]));*/
+//	/*int a = 0;
+////	int b = 20;
+//	if (a && b)
+//	{
+//		printf("hehe\n");
+//	}*/
+//	////int a = 20;/*
+//	//int b = 30;
+	//int r = (a > b ? a : b);*/
+//	int a = 2; 
+//	int b = 4;
+//	int c = 9;
+//	int r = (c = a - 2, b = a + c, a + c);
+//	printf("%d", r);
+//	return 0;
+//}
+
+//typedef unsigned int uint;
+//typedef struct Node
+//{
+//	int date;
+//	struct Node* next;
+//}Node;
+//int main()
+//{
+//	unsigned int num = 0;
+//	uint num2 = 2;
+//	struct Node n;
+//	Node n2;
+//	return 0;
+//}
+#include <stdio.h>
+
+//void test()
+//{
+//	static int a = 1;
+//	a++;
+//	printf("%d ", a);
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		test();
+//		i++;
+//	}
+//	return 0;
+//}
+//static int a = 2;
+//static int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int main()
+//{
+//	register int a = 10;//建议变量存放在寄存器中
+//	return 0;
+//}
+//#define a 10
+//#define ADD(x,y) ((x)+(y))
+//int main()
+//{
+//	//printf("%d\n", a);	
+//	//int b = a;
+//	//printf("%d\n", b);
+//	//int arr[a] = { 0 };
+//	int a = 10;
+//	int b = 20;
+//	int r = ADD(a, b);
+//	printf("%d", r);
+//	return 0;
+////} 
+//int main()
+//{
+//	//int a = 10;
+//	//printf("%p\n", &a); //000000968E71F6B4
+//	//int* p = &a;
+//	//char pa = 'a';
+	//char* ca = &pa;
+	//*p = 20;//解引用操作符
+//	//printf("%d\n", a);
+//	printf("%zu\n", sizeof(char*));
+//	printf("%zu\n", sizeof(int*));
+//	printf("%zu\n", sizeof(short*));
+//	printf("%zu\n", sizeof(float*));
+//	printf("%zu\n", sizeof(double*));
+//
+//	return 0;
+//}
